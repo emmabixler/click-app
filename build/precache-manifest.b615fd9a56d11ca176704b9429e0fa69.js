001c828418cab5dce64d2b046fc51ad2728fa01f
leb <leb5313@gmail.com>
2019-06-06 15:39:42 -0400

    created new repo, uploaded all the packages

---
+++ build/precache-manifest.b615fd9a56d11ca176704b9429e0fa69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf0138c76bf843a3acd3b0cc501e67e",
+    "url": "/emmabixler/clicky-game.git/index.html"
+  },
+  {
+    "revision": "982c6b9414ef253b935c",
+    "url": "/emmabixler/clicky-game.git/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/emmabixler/clicky-game.git/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "982c6b9414ef253b935c",
+    "url": "/emmabixler/clicky-game.git/static/js/main.533cfad4.chunk.js"
+  },
+  {
+    "revision": "32caea74cbc3b171cf66",
+    "url": "/emmabixler/clicky-game.git/static/js/runtime~main.bb75ce9e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/emmabixler/clicky-game.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
