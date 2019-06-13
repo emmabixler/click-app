9c0114081efc9ca919627fedd48e8f1fd368a5bf
leb <leb5313@gmail.com>
2019-06-13 18:13:36 -0400

    updated characters card

--- components/card/card.js
+++ components/card/card.js
@@ -1,1 +1,10 @@
-export { default } from "./card";
+import React from "react";
+import "../card.css";
+
+const card = props => (
+  <div className="card" onClick={e => props.clickEvent(e.target.src)}>
+    <img className="card-img-top card height" src={props.name} alt="" />
+  </div>
+);
+
+export { default } from "card";
--- components/card/index.js
+++ components/card/index.js
@@ -1,15 +1,1 @@
-import React from "react";
-import card from "./card.css";
-
-const card = props => (
-  <div className="card" onClick={props.imageClick}>
-    <div className="img-container">
-      <img
-        alt={props.image.replace(".jpg", "")}
-        src={require("../../images/" + props.image)}
-      />
-    </div>
-  </div>
-);
-
-export default card;
+export { default } from "./card";
